d0be6d20581d819df88b866791b8a425b199b727
Junseo Rho <joonseor@gmail.com>
2025-06-19 10:35:24 +0900

    Clearance for Refactoring
    Start with 3 Core Scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -49,7 +49,7 @@ public class GameManager : MonoBehaviour
         }
         else
         {
-            wormManager.LoadCurrentWorm(saveData.wormList[^1]); // 가장 마지막 웜 로드
+            WormManager.LoadCurrentWorm(saveData.wormList[^1]); // 가장 마지막 웜 로드
         }
 
         UpdateCurrentWormNameUI();
